f04ddf5fc5feb7292a0a84139a33f79b0447073b
SiEsol <rangga1874@gmail.com>
2023-12-17 02:20:28 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,123 @@
-import Image from 'next/image'
+'use client'
 
-export default function Home() {
+import { useState } from "react";
+
+function CalculatorFlipFlop() {
+  const [type, setType] = useState("");
+  const [set, setSet] = useState(""); 
+  const [reset, setReset] = useState("");
+  const [q, setQ] = useState("");
+  const [qNot, setQNot] = useState("");
+
+  const calculate = () => {
+
+    let outputQ, outputQNot;
+  
+    if (type === "NAND") {
+      if (set === "1" && reset === "1") {
+        outputQ = 1;
+        outputQNot = 1; 
+      }
+      else if (set === "0" && reset === "1") {
+        outputQ = 1;
+        outputQNot = 0;
+      }
+      else if (set === "1" && reset === "0") {
+        outputQ = 0;
+        outputQNot = 1; 
+      }
+      else {
+        outputQ = "X";  
+        outputQNot = "X";
+      }
+    }
+  
+    else if (type === "NOR") {
+      if (set === "1" && reset === "1") {
+        outputQ = "X";
+        outputQNot = "X";
+      }
+      else if (set === "0" && reset === "1") {
+        outputQ = 0; 
+        outputQNot = 1;
+      }
+      else if (set === "1" && reset === "0") {
+        outputQ = 1;
+        outputQNot = 0;
+      }
+      else {
+        outputQ = 0;  
+        outputQNot = 0;
+      }
+    }
+  
+    setQ(String(outputQ));
+    setQNot(String(outputQNot)); 
+  }
+  
+  const validateInput = (value: string) => {
+    if(value.length === 0 || (value === "0" || value === "1")) {
+      return true;
+    }
+  
+    return false; 
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className="h-screen bg-white justify-center flex items-center">
+        <div className="flex justify-center h-[600px] w-[600px] shadow-[0_0_15px_-4px_rgba(0,0,0,0.25)] rounded-[40px]">
+          <div className="flex flex-col justify-center">
+              <div className="flex justify-center items-center flex-col">
+                <p className="text-black font-bold font-['FONTSPRING DEMO - Greycliff CF'] text-[30px]">Hello!</p>
+                <p className="text-black font-normal font-['DM Sans'] text-[15px] text-slate-400">Calculator Flip-Flop</p>
+              </div>
+              <p className="text-black w-[80px] font-['DM Sans'] mt-[30px]">Menu</p>
+              <select className="flex justify-center items-center text-slate-700 italic text-[15px] w-96 outline-none mb-[30px] h-[40px] shadow-[inset_0_0_3px_0px_rgba(0,0,0,0.25)] rounded-xl px-[10px] bg-gray-50" onChange={(e) => setType(e.target.value)}>
+                  <option value="" disabled selected hidden>Select one..</option>
+                  <option value="NAND">NAND</option>
+                  <option value="NOR">NOR</option>
+              </select>
+              <div className="flex flex-col my-[5px]">
+                <p className="text-black w-[80px] font-['DM Sans']">Set</p>
+                <input className="text-black w-[100%] h-[40px] text-[14px] bg-gray-50 rounded-xl shadow-[inset_0_0_3px_0px_rgba(0,0,0,0.25)] outline-none placeholder:italic pl-[10px]"
+                placeholder="Masukkan angka 1 atau 0"
+                    value={set}
+                    onChange={e => {
+                    if(validateInput(e.target.value)) { 
+                        setSet(e.target.value)
+                    }
+                    }}  
+                />
+              </div>
+              <div className="flex flex-col my-[5px]">
+                <p className="text-black font-['DM Sans'] w-[80px]">Reset</p>
+                <input className="text-black w-[100%] h-[40px] text-[14px] bg-gray-50 rounded-xl shadow-[inset_0_0_3px_0px_rgba(0,0,0,0.25)] outline-none placeholder:italic pl-[10px]"
+                placeholder="Masukkan angka 1 atau 0"
+                    value={reset}
+                    onChange={e => {
+                    if(validateInput(e.target.value)) {
+                        setReset(e.target.value)  
+                    } 
+                    }}    
+                />
+              </div>
+              <div className="flex items-center justify-center my-[10px]">
+                <button className="hover:bg-sky-400 hover:duration-300 ease-in-out duration-300 flex text-white font-semibold font-['FONTSPRING DEMO - Greycliff CF Heavy'] items-center justify-center bg-sky-600 rounded-[30px] w-[100%] h-[50px]" onClick={calculate}>Calculate!</button> 
+              </div>
+              <p className="text-slate-700 mt-[20px]">Result</p>
+              <div className="text-black flex flex-row justify-between mt-[20px] text-[25px] font-['FONTSPRING DEMO - Greycliff CF Heavy'] font-bold text-slate-700 justify-center items-center">
+                <div className="flex flex-row justify-center items-center">
+                  Q = <div className="mx-[15px] w-[100px] h-[40px] shadow-[inset_0_0_3px_0px_rgba(0,0,0,0.25)] rounded-xl text-[15px] justify-center items-center flex font-semibold">{q}</div>
+                </div>
+                <div className="flex flex-row justify-center items-center">
+                  Q' = <div className="mx-[15px] w-[100px] h-[40px] shadow-[inset_0_0_3px_0px_rgba(0,0,0,0.25)] rounded-xl text-[15px] justify-center items-center flex font-semibold">{qNot}</div>
+                </div>
+              </div>
+              
+          </div>
+          </div>   
+    </div>
+  );
 }
+
+export default CalculatorFlipFlop;
